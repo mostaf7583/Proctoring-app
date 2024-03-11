80ab265f631b33fe680defa851df82eb19d3fe83
mostaf7583 <73074293+mostaf7583@users.noreply.github.com>
2024-03-11 05:12:58 +0200

    Update logo image in Navbar component

--- my-first-react-app/src/Navbar.js
+++ my-first-react-app/src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
       <nav className="navbar">
-        <img src="https://images.app.goo.gl/hvGwCUxB8jp8tvXa6" alt="logo" />
+        <img src="https://www.guc.edu.eg/img/guc_logo.png" alt="logo" />
         <div className="links">
         </div>
       </nav>
