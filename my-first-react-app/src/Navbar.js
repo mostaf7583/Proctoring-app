5347a267a764cdb584fce5eeee7fd2f07f2eaa55
mostaf7583 <73074293+mostaf7583@users.noreply.github.com>
2024-03-11 03:07:17 +0000

    Add dependencies and update styling

---
+++ my-first-react-app/src/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+      <nav className="navbar">
+        <img src="https://images.app.goo.gl/hvGwCUxB8jp8tvXa6" alt="logo" />
+        <div className="links">
+        </div>
+      </nav>
+    );
+  }
+   
+  export default Navbar;
